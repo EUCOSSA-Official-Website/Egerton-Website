b9ca45597e585e62a571981dea1082abd817cec3
NjauSamuel <njaus602@gmail.com>
2024-11-19 11:30:15 +0300

    Fixed a bug that prevented a user from accessing an individual event if their unauthenticated.

--- resources/js/Pages/Events/Show.vue
+++ resources/js/Pages/Events/Show.vue
@@ -108,13 +108,13 @@
                     Hackathon
                 </span>
 
-                <div v-if="user.role === 'admin'" class="flex justify-between sm:text-xl mt-5 items-center">
+                <div v-if="user?.role === 'admin'" class="flex justify-between sm:text-xl mt-5 items-center">
                     <Link v-if="props.event.event_charge" class="text-3xl" :href="route('event-payment', {event: props.event.id})" as="button" method="post"><PrimaryButton>Get Ticket</PrimaryButton></Link>
                     <Link href="#" ><PrimaryButton  @click="copyLinkToClipboard" class="bg-white hover:bg-slate-200 text-black" as="button">Share Event</PrimaryButton></Link>
                     <Link :href="route('events.destroy', {event: props.event.id})" as="button" method="delete"><PrimaryButton class="bg-red-600 hover:bg-red-700">Delete Event</PrimaryButton></Link>
                 </div>
                 <div v-else class="flex justify-between mt-5">
-                    <Link v-if="props.event.event_charge" class="text-3xl" :href="route('event-payment')" as="button" method="post"><PrimaryButton>Get Ticket</PrimaryButton></Link>
+                    <Link v-if="props.event.event_charge" class="text-3xl" :href="route('event-payment', {event: props.event.id})" as="button" method="post"><PrimaryButton>Get Ticket</PrimaryButton></Link>
                     <SecondaryButton  @click="copyLinkToClipboard" class="text-3xl">Share Event</SecondaryButton>
                 </div>
 
