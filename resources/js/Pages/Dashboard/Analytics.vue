d9eed017e6ff6e3c207a0fe47529cc5faaca899b
NjauSamuel <njaus602@gmail.com>
2025-03-28 15:17:58 +0300

    added sorting to the data on the analytics page tables.

--- resources/js/Pages/Dashboard/Analytics.vue
+++ resources/js/Pages/Dashboard/Analytics.vue
@@ -81,7 +81,7 @@
     const tableOptions = {
         paging: true,
         searching: true,
-        ordering: false,
+        ordering: true,
         pageLength: 10,
         lengthChange: true,
         dom: "lfrtip",
