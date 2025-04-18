9e6b8d620f13334aafad6b566c981461ef207997
NjauSamuel <njaus602@gmail.com>
2025-04-18 10:07:09 +0300

    Updated the frontends pusher fetch.

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -1,11 +1,12 @@
 import Echo from 'laravel-echo';
-
 import Pusher from 'pusher-js';
+
 window.Pusher = Pusher;
 
 window.Echo = new Echo({
     broadcaster: 'pusher',
-    key: import.meta.env.VITE_PUSHER_APP_KEY,
-    cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
-    forceTLS: true
+    key: 'dc7341bc673250f3fc36',
+    cluster: 'mt1',
+    forceTLS: true,
+    encrypted: true, // optional, but safe to include
 });
